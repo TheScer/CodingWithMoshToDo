768d93054a87c1991b2a3485ff822f266fef2fde
Oscar Hero <thescer6@gmail.com>
2022-02-04 09:40:01 +0700

    learning about variable sizes so it fits on the screen

--- ExpoProject/App.js
+++ ExpoProject/App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+
+} from 'react-native';
+
+//in this video https://www.youtube.com/watch?v=0-S5a0eXPoc&t=4136s&ab_channel=ProgrammingwithMosh
+//we're gonna start building our app pages and learn
+// Dimensions
+// Device orientationflexbox
+// absolute and relative positioning
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.AndroidSafeArea}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: '50%',
+        height: 70,
+      }}></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +31,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  AndroidSafeArea: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
+ 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CodingWithMoshToDo
+
+//in this video https://www.youtube.com/watch?v=0-S5a0eXPoc&t=4136s&ab_channel=ProgrammingwithMosh
+//we're gonna start building our app pages and learn
+// Dimensions
+// Device orientationflexbox
+// absolute and relative positioning
